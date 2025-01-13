bc2d14827ccba93d4215406e644e3897a150c629
almor <lifergamerus@gmail.com>
2025-01-14 03:06:05 +0500

    [fix] downloader

--- bot/utils/log.py
+++ bot/utils/log.py
@@ -1,5 +1,20 @@
-from datetime import datetime
+import logging
 
+# Настройка логгера
+logger = logging.getLogger("BotLogger")
+logger.setLevel(logging.INFO)  # Можно поменять на DEBUG для детальных логов
+
+# Обработчик для вывода в консоль
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+
+# Формат логов
+formatter = logging.Formatter('[%(asctime)s] %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+console_handler.setFormatter(formatter)
+
+# Добавляем обработчик к логгеру
+logger.addHandler(console_handler)
+
+# Функция логирования
 def log_action(action, details=""):
-    time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    print(f"[{time}] {action}: {details}")
+    logger.info(f"{action}: {details}")
