a01ac35242da5a282852b1d92f1e796eabe8bd9a
almor <lifergamerus@gmail.com>
2025-01-19 01:22:06 +0500

    [fix] downloader[fix] downloader[fix] downloader[fix] downloader[fix] downloader[fix] downloader[fix] downloader[fix] downloader

--- bot/main.py
+++ bot/main.py
@@ -1,4 +1,5 @@
 import asyncio
+import requests
 from bot.handlers.start_handler import start
 from bot.handlers.video_handler import handle_link, handle_quality_selection
 from bot.utils.log import log_action
@@ -7,10 +8,16 @@ from config import bot, dp
 
 async def main():
     try:
+        # Получаем и выводим внешний IP
+        ip = requests.get('https://api.ipify.org').text
+        log_action("Внешний IP сервера", ip)
+
         check_ffmpeg_installed()
     except EnvironmentError as e:
         log_action("Ошибка запуска", str(e))
         exit(1)
+    except Exception as e:
+        log_action("Ошибка получения IP", str(e))
 
     # Регистрация хендлеров
     dp.message.register(start, lambda msg: msg.text == "/start")
