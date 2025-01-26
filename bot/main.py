313ed3039962b0365976eab378314319032c604b
almor <lifergamerus@gmail.com>
2025-01-27 00:14:37 +0500

    [fix] downloader[fix] downloader[fix] downloader[fix] downloader[fix] downloader[fix] downloader[fix] downloader[fix] downloader

--- bot/main.py
+++ bot/main.py
@@ -1,27 +1,25 @@
 import asyncio
-from bot.handlers.start_handler import start
-from bot.handlers.video_handler import handle_link, handle_quality_selection
+import subprocess
 from bot.utils.common import get_external_ip
 from bot.utils.log import log_action
-from bot.utils.downloader import check_ffmpeg_installed
-from config import bot, dp
 
-async def main():
-    try:
-        # Получение и логирование IP
-        get_external_ip()
-        check_ffmpeg_installed()
-    except EnvironmentError as e:
-        log_action("Ошибка запуска", str(e))
-        exit(1)
 
-    # Регистрация хендлеров
-    dp.message.register(start, lambda msg: msg.text == "/start")
-    dp.message.register(handle_link, lambda msg: msg.text.startswith("http"))
-    dp.message.register(handle_quality_selection, lambda msg: "p" in msg.text or msg.text == "Только аудио")
+async def reconnect_vpn():
+    while True:
+        try:
+            # Получение и логирование IP
+            ip = get_external_ip()
+            log_action("Внешний IP сервера:", ip)
 
-    log_action("Бот запущен")
-    await dp.start_polling(bot)
+            # Переподключение к NordVPN
+            subprocess.run(["nordvpn", "disconnect"], check=True)
+            subprocess.run(["nordvpn", "connect"], check=True)
+            log_action("VPN переподключен")
+        except Exception as e:
+            log_action("Ошибка при переподключении VPN", str(e))
+
+        await asyncio.sleep(300)  # Повторяем каждые 5 минут
+
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(reconnect_vpn())
