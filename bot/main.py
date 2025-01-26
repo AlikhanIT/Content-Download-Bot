1c594811b2285db0409e1173e2a24d4927c15cf9
almor <lifergamerus@gmail.com>
2025-01-27 00:24:18 +0500

    [fix] downloader[fix] downloader[fix] downloader[fix] downloader[fix] downloader[fix] downloader[fix] downloader[fix] downloader

--- bot/main.py
+++ bot/main.py
@@ -3,7 +3,6 @@ import subprocess
 from bot.utils.common import get_external_ip
 from bot.utils.log import log_action
 
-
 async def reconnect_vpn():
     while True:
         try:
@@ -11,15 +10,25 @@ async def reconnect_vpn():
             ip = get_external_ip()
             log_action("Внешний IP сервера:", ip)
 
+            # Проверка наличия запущенной службы NordVPN
+            service_check = subprocess.run(
+                ["nordvpn", "status"], capture_output=True, text=True
+            )
+            if "You are not connected to NordVPN" in service_check.stdout or "nordvpnd.sock not found" in service_check.stderr:
+                log_action("NordVPN не запущен, пытаемся стартовать службу...")
+                subprocess.run(["/etc/init.d/nordvpn", "start"], check=True)
+                await asyncio.sleep(5)
+
             # Переподключение к NordVPN
             subprocess.run(["nordvpn", "disconnect"], check=True)
             subprocess.run(["nordvpn", "connect"], check=True)
             log_action("VPN переподключен")
-        except Exception as e:
+        except subprocess.CalledProcessError as e:
             log_action("Ошибка при переподключении VPN", str(e))
+        except Exception as e:
+            log_action("Неизвестная ошибка VPN", str(e))
 
         await asyncio.sleep(300)  # Повторяем каждые 5 минут
-
 
 if __name__ == "__main__":
     asyncio.run(reconnect_vpn())
