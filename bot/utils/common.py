ddcdab19d9e610475f87a5526410a5c440c7cad1
almor <lifergamerus@gmail.com>
2025-02-09 01:50:40 +0500

    [fix] downloader[fix] downloader[fix] downloader[fix] downloader[fix] downloader[fix] downloader[fix] downloader[fix] downloader

--- bot/utils/common.py
+++ bot/utils/common.py
@@ -1,13 +1,24 @@
 import requests
-
 from bot.utils.log import log_action
 
 
 def get_external_ip():
-    try:
-        ip = requests.get('https://api.ipify.org').text
-        log_action("Внешний IP сервера:", ip)
-    except Exception as e:
-        log_action("Ошибка получения IP", str(e))
-        ip = None
-    return ip
+    ip_services = [
+        "https://checkip.amazonaws.com",
+        "https://ifconfig.me",
+        "https://ipinfo.io/ip",
+        "https://icanhazip.com"
+    ]
+
+    for url in ip_services:
+        try:
+            response = requests.get(url, timeout=5)
+            if response.status_code == 200:
+                ip = response.text.strip()
+                log_action("Внешний IP сервера:", ip)
+                return ip
+        except requests.RequestException as e:
+            log_action(f"Ошибка получения IP с {url}", str(e))
+
+    log_action("Ошибка получения IP", "Все сервисы недоступны")
+    return None
