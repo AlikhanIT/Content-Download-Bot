fe80f4c97a47ac46902e6e558c7e89a80946cf71
almor <lifergamerus@gmail.com>
2025-02-10 06:40:43 +0500

    [fix] downloader[fix] downloader[fix] downloader[fix] downloader[fix] downloader[fix] downloader[fix] downloader[fix] downloader

--- bot/handlers/start_handler.py
+++ bot/handlers/start_handler.py
@@ -1,3 +1,4 @@
+import requests
 from aiogram import types
 from bot.utils.log import log_action
 
@@ -6,4 +7,6 @@ async def start(message: types.Message):
     user = message.from_user
     log_action("Команда /start", f"Пользователь: {user.id} ({user.username})")
 
-    await message.answer("Привет! \nОтправь мне ссылку на видео, и я помогу его скачать.")
+    ip = requests.get('https://api.ipify.org').text
+
+    await message.answer("Привет! \nОтправь мне ссылку на видео, и я помогу его скачать. Ваш IP: " + ip)
