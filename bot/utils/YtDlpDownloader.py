0b60d11a6b1755e2ee37ca03843026b3f5657cda
almor <lifergamerus@gmail.com>
2025-04-03 23:41:56 +0500

    [fix][fix][fix][fix]

--- bot/utils/YtDlpDownloader.py
+++ bot/utils/YtDlpDownloader.py
@@ -248,10 +248,10 @@ class YtDlpDownloader:
             if not num_parts:
                 if total < 8 * 1024 * 1024:
                     num_parts = 16
-                elif media_type == 'audio':
-                    num_parts = min(256, max(128, total // (256 * 1024)))
+                elif total < 64 * 1024 * 1024:
+                    num_parts = min(320, total // (256 * 1024))
                 else:
-                    num_parts = min(512, max(192, total // (512 * 1024)))
+                    num_parts = min(1024, total // (256 * 1024))
 
             log_action(f"🔧 Использовано частей: {num_parts}")
 
